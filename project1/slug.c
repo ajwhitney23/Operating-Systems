c7e39ea92acf01a12151de0319aebc111536b944
Nathan Bargman <nathan.bargman15@gmail.com>
2021-02-08 13:45:24 -0500

    comments

--- project1/slug.c
+++ project1/slug.c
@@ -5,31 +5,38 @@
 
 int main( int argc, char *argv[] ) {
 
+    //make sure the right number of args is entered
     if(argc != 2){return 0;}
 
+    //create seed file name
     char* filename = malloc(16);
     strcat(filename, "seed_slug_");
     strcat(filename, argv[1]);
     strcat(filename, ".txt");
 
+    //read file
     char* buffer = malloc(20);
     FILE *fp = fopen(filename, "r");
     fgets(buffer, sizeof(buffer), fp);
     fclose(fp);
     printf("Read seed value: %s\n", buffer);
 
+    //set seed and make random numbers
     srand(atoi(buffer));
     int wait_time = rand() % 4 + 1;
     int coin_flip = rand() % 2;
 
+    //delay
     printf("Delay time is %d seconds. Coin flip: %d\n", wait_time, coin_flip);
     printf("I'll get the job done. Eventually...\n");
     sleep(wait_time);
     printf("Break time is over! I am running the '");
 
+    //create args for exec
     char *args_0[]={"last", "-d", "--fulltimes", NULL};
     char *args_1[]={"id", "-u", NULL};
 
+    //call exec based on coin flip
     if(coin_flip == 0){
         printf("last -d --fulltimes' command.\n");
         execvp(args_0[0], args_0);
--- project1/slugrace.c
+++ project1/slugrace.c
@@ -10,31 +10,42 @@ double get_seconds_past(struct timespec start);
 
 int main() {
 
+    //start clock
     struct timespec start;
     clock_gettime(CLOCK_REALTIME, &start);
 
+    //make variables for holding slug info
     int slug_pid[4];
     struct timespec slug_time[4];
     int done_cnt = 0;
 
+    //fork off 4 slugs
     for(int i = 1; i < 5; i++){
         int rc = fork();
-        if(rc == 0){
+        if(rc == 0){ //child only
+
+            //create args for exec
             char number[2];
             sprintf(number, "%d", i);
             char *args[]={"./slug", number, NULL};
 
+            //call exec
             printf("[Child PID: %d]: Executing '%s %s' command...\n", getpid(), args[0], args[1]);
             execv(args[0], args);
 
-        }else{
+        }else{ //parent only
+
+            //keep track of slug variables
             printf("[Parent]: I forked off child %d\n", rc);
             slug_pid[i - 1] = rc;
             clock_gettime(CLOCK_REALTIME, &slug_time[i - 1]);
         }
     }
 
+    //waiting for slugs
     while(done_cnt < 4){
+
+        //print out which slugs are still racing
         printf("The race is still going. The following children are still racing: ");
         for(int i = 0; i < 4; i++){
             if(slug_pid[i] > 0){
@@ -43,10 +54,12 @@ int main() {
         }
         printf("\n");
 
+        //check to see if slugs are still running
         for(int i = 0; i < 4; i++){
             if(slug_pid[i] > 0){
                 pid_t return_pid = waitpid(slug_pid[i], NULL, WNOHANG);
                 if (return_pid == slug_pid[i]) {
+                    //note slug as ended
                     printf("Child %d has crossed the finish line! It took %f seconds\n", slug_pid[i], get_seconds_past(slug_time[i]));
                     slug_pid[i] = 0;
                     done_cnt ++;
@@ -54,13 +67,19 @@ int main() {
             }
         }
 
+        //pause between prints
         usleep(250000);
     }
 
+    //race end
     printf("The race is over! It took %f seconds\n", get_seconds_past(start));
     return 0;
 }
 
+/**
+ * @param start: any timespec (from time.h)
+ * @return time passed since start in seconds
+ */
 double get_seconds_past(struct timespec start){
     struct timespec end;
     clock_gettime(CLOCK_REALTIME, &end);
